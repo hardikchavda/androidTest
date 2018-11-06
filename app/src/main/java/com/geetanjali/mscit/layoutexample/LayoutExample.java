c533d218eb996950f32b7322d0fa2916eb109efa
Hardik Chavda <hardikkchavda@gmail.com>
2018-11-06 11:08:06 +0530

    First Test

---
+++ app/src/main/java/com/geetanjali/mscit/layoutexample/LayoutExample.java
@@ -0,0 +1,15 @@
+package com.geetanjali.mscit.layoutexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LayoutExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_example);
+
+
+    }
+}
